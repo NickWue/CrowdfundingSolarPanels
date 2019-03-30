f58557bec69023469b8d6767e48324a94805bfa4
Heidi <adelaida.creosteanu@gmail.com>
2019-03-30 15:03:35 +0000

    [Feat] Investor dashboard
    Template card of project and new project button

---
+++ crowdsolar/src/Components/InvestorDashboard.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {Card, ListGroup, ListGroupItem, Button} from 'react-bootstrap/';
+import solarpanel from './solarpanel.jpeg';
+
+class InvestorDashboard extends Component {
+    render() {
+      const pageStyle = {
+          paddingTop:"100px",
+          // display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-around',
+          overflow: 'hidden',
+          paddingRight: "10%",
+          paddingLeft: "10%"
+      }
+  
+      return (
+        <div style={pageStyle}>
+          <h1>Your current projects</h1>
+          <Card style={{width: '20rem'}}>
+            <Card.Img variant="top" src={solarpanel}/>
+            <Card.Body>
+              <Card.Title>Morocco</Card.Title>
+              <Card.Text>This is a description</Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>Status: <mark style={{color: 'Orange'}}>Building</mark></ListGroupItem>
+              <ListGroupItem>Return on investment: 10% per year</ListGroupItem>
+            </ListGroup>
+          </Card>
+          <br/>
+          <Button variant="outline-primary" size="lg" type="submit">Find new project</Button>
+        </div>
+      );
+    }
+}
+
+export default InvestorDashboard;
