5ceb41b8ea9e8a259e3dc682cefa2c2d9d9d08b7
Christopher Coles <cbc1g16@soton.ac.uk>
2019-03-30 13:51:23 +0100

    [Feat] added nav bar

--- crowdsolar/src/App.js
+++ crowdsolar/src/App.js
@@ -2,14 +2,25 @@ import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Details from './Components/Details'
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+
 class App extends Component {
   render() {
+    const left= {
+      float: "left",
+     
+    }
     return (
-      <div>
-        <Details></Details>
-  fdsadsafdsfdsafdsa fdsafasd
-        <Details></Details>
-      </div>
+      <AppBar  position="fixed">
+        <Toolbar>
+                  <Button  href="/" color="inherit">Home</Button>
+                  <Button color="inherit" style={{marginLeft: "80%"}}  href="/login">Login</Button>
+                  
+        </Toolbar>
+      </AppBar>
+      
     );
   }
 }
