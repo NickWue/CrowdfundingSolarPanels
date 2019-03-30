b1849aca904d17022ce5701e4071c968ab1ff536
Christopher Coles <cbc1g16@soton.ac.uk>
2019-03-30 12:54:41 +0100

    [Feat] button example

---
+++ crowdsolar/src/Components/Details.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import Button from 'react-bootstrap/Button';
+
+class Details extends Component {
+  render() {
+    return (
+
+      <Button>fdsajfl</Button>
+
+    );
+  }
+}
+
+export default Details;
