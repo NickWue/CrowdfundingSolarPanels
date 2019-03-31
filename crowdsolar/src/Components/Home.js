5147baf11b28b1460b67a601d0206b728ae70d47
Christopher Coles <cbc1g16@soton.ac.uk>
2019-03-31 09:27:53 +0200

    [feat] add user to database

--- crowdsolar/src/Components/Home.js
+++ crowdsolar/src/Components/Home.js
@@ -9,7 +9,47 @@ class Home extends Component {
     super(props);
     this.state = {};
   }
+  checkUserStatus = email => {
+    var userFound = false
+    this.props.users.forEach(user => {
+      if(user.idusers === email){
+        userFound = true
+      }
+    });
+    return userFound
+  }
 
+  addUser = (type, description) => {
+    fetch('http://x10z.de/crowdsolar/addUser/' +
+    '?gid='  + this.props.email +
+    '&name=' + this.props.name +
+    '&type=' + type +
+    '&desribtion=' + description
+  );
+  }
+
+  sendProject = (project, lat, lng) => {
+    fetch('http://x10z.de/crowdsolar/addProject/' +
+      '?name=' + project.name +
+      '&street=nameofthestreet2' +
+      '&countryID=' + project.country +
+      '&dimX=' + project.length +
+      '&dimY=' + project.width +
+      '&userid=' + this.props.email +
+      '&funding_required=5000' +
+      '&funding_recieved=0' +
+      '&expectedreturn=8'
+    )
+  }
+
+  getInitial = () =>{
+    return (
+      <Fragment>
+        <Button onClick={() => this.addUser(2, "Investor")} href="/investor" variant="outline-primary" size="lg">Join as an Investor</Button>
+        <Button onClick={() => this.addUser(1, "Landowner")} href="/landowner" variant="outline-primary" size="lg">Join as a Landowner</Button>
+      </Fragment>
+    )
+  }
   render() {
     const pageStyle = {
         paddingTop:"100px",
@@ -39,7 +79,7 @@ class Home extends Component {
         
       </Container>
       <div style={pageStyle}>
-      {this.props.loggedIn ? <Button>Go to dashboard</Button>: <p>Please use the login button in the top right</p>}
+      {this.props.loggedIn ? this.checkUserStatus(this.props.email) ? <Button href={() => this.getDash()}>Go to dashboard</Button> : this.getInitial() : <p>Please use the login button in the top right</p>}
      </div>
      </Fragment>
     );
